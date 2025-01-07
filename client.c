948a9c851697097bf7f30eae57d6ae5e6a8f4692
Lawrence Atkins <lawrenceatkins1999@gmail.com>
2025-01-07 09:13:00 +0000

    Refactor client slightly

--- client.c
+++ client.c
@@ -7,6 +7,48 @@
 #include <netinet/ip.h>
 #include <arpa/inet.h>
 
+void send_message(
+    int sockfd,
+    const char* dest_ip, 
+    const char* message
+) {
+    // Prepare IP header
+    char datagram[4096];
+    struct ip *ip_header = (struct ip *)datagram;
+    
+    // Zero out the packet buffer
+    memset(datagram, 0, 4096);
+
+    // Fill in the IP header
+    ip_header->ip_hl = 5;    // 5 * 32-bit words = 20 bytes
+    ip_header->ip_v = 4;     // IPv4
+    ip_header->ip_tos = 0;
+    ip_header->ip_len = sizeof(struct ip) + strlen(message);
+    ip_header->ip_id = htons(54321);
+    ip_header->ip_off = 0;
+    ip_header->ip_ttl = 64;  // Time-To-Live
+    ip_header->ip_p = IPPROTO_RAW;
+    ip_header->ip_sum = 0;   // Kernel will fill in if left at 0
+    ip_header->ip_src.s_addr = inet_addr("192.168.1.2");  // Source IP (change as needed)
+    ip_header->ip_dst.s_addr = inet_addr(dest_ip);        // Destination IP
+    strcpy(datagram + sizeof(struct ip), message); // Add payload to end of ip_header
+
+    // Prepare destination address
+    struct sockaddr_in dest_addr;
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_addr.s_addr = inet_addr(dest_ip);
+
+    // Send the packet
+    if (sendto(sockfd, datagram, ip_header->ip_len, 0,
+               (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0) {
+        perror("sendto failed");
+        exit(1);
+    }
+
+    printf("Sent message to %s\n", dest_ip);
+
+}
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <destination_ip> <message>\n", argv[0]);
@@ -27,44 +69,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    // Prepare IP header
-    char datagram[4096];
-    struct ip *ip_header = (struct ip *)datagram;
-    
-    // Zero out the packet buffer
-    memset(datagram, 0, 4096);
-
-    // Fill in the IP header
-    ip_header->ip_hl = 5;    // 5 * 32-bit words = 20 bytes
-    ip_header->ip_v = 4;     // IPv4
-    ip_header->ip_tos = 0;
-    ip_header->ip_len = sizeof(struct ip) + strlen(argv[2]);
-    ip_header->ip_id = htons(54321);
-    ip_header->ip_off = 0;
-    ip_header->ip_ttl = 64;  // Time-To-Live
-    ip_header->ip_p = IPPROTO_RAW;
-    ip_header->ip_sum = 0;   // Kernel will fill in if left at 0
-    ip_header->ip_src.s_addr = inet_addr("192.168.1.2");  // Source IP (change as needed)
-    ip_header->ip_dst.s_addr = inet_addr(argv[1]);        // Destination IP
-
-    // Add the payload (message)
-    strcpy(datagram + sizeof(struct ip), argv[2]);
-
-    // Prepare destination address
-    struct sockaddr_in dest_addr;
-    memset(&dest_addr, 0, sizeof(dest_addr));
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_addr.s_addr = inet_addr(argv[1]);
-
-    // Send the packet
-    if (sendto(sockfd, datagram, ip_header->ip_len, 0,
-               (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0) {
-        perror("sendto failed");
-        exit(1);
-    }
-
-    printf("Sent message to %s\n", argv[1]);
-
+    send_message(sockfd, argv[1], argv[2]);
     close(sockfd);
     return 0;
 }
