b1e8f42126ae329c7b01b5cdf24f3a817ebe2b81
Lawrence Atkins <lawrenceatkins1999@gmail.com>
2025-01-14 09:11:38 +0000

    Slight improvement to create_ip_socket()

--- src/ip/utils.h
+++ src/ip/utils.h
@@ -1,3 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +9,7 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
 #define MAX_DATAGRAM_SIZE 4096
 
@@ -13,16 +17,17 @@ int create_ip_socket() {
     int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
     if (sockfd < 0) {
         perror("socket creation failed");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     int one = 1;
     if (setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
         perror("setsockopt failed");
-        exit(1);
+        close(sockfd);
+        exit(EXIT_FAILURE);
     }
 
     return sockfd;
 }
 
-
+#endif
