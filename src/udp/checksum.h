18ce6db3bda8c4384493216109de5548af33185e
Lawrence Atkins <lawrenceatkins1999@gmail.com>
2025-01-24 12:10:40 +0000

    Run linting

--- src/udp/checksum.h
+++ src/udp/checksum.h
@@ -1,13 +1,13 @@
-#include <stdint.h>
 #include "types.h"
 #include <netinet/ip.h>
+#include <stdint.h>
 
 typedef struct __attribute__((packed)) {
-        uint32_t src_ip;
-        uint32_t dest_ip;
-        uint8_t zero;
-        uint8_t protocol;
-        uint16_t udp_length;
-    } udp_pseudo_header;
+  uint32_t src_ip;
+  uint32_t dest_ip;
+  uint8_t zero;
+  uint8_t protocol;
+  uint16_t udp_length;
+} udp_pseudo_header;
 
-uint16_t calculate_udp_checksum(ip* ip_header, udp_datagram* udp_packet);
+uint16_t calculate_udp_checksum(ip *ip_header, udp_datagram *udp_packet);
