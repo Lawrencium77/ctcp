da2a7d7f170a9830b1717c0d5b6e4616d1751a4b
Lawrence Atkins <lawrenceatkins1999@gmail.com>
2025-01-24 12:50:24 +0000

    Fix compiler error

--- src/udp/checksum.h
+++ src/udp/checksum.h
@@ -2,7 +2,9 @@
 #include <netinet/ip.h>
 #include <stdint.h>
 
-typedef struct __attribute__((packed)) {
+// Align entire struct on 2-byte boundary to allow udp_pseudo_header* conversion
+// to uint16_t*
+typedef struct __attribute__((packed, aligned(2))) {
   uint32_t src_ip;
   uint32_t dest_ip;
   uint8_t zero;
